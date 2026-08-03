2460416cb40940ff18b092107691b9d0a4f66ee3
FelipeGaspar22 <estudos.felipe.gaspar@gmail.com>
2026-08-03 08:43:13 -0300

    Update Hello Word.c

--- Hello Word.c
+++ Hello Word.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
+// O comando "#include <stdio.h>" está trazendo uma biblioteca, que permite o uso das funções básicas da linguagem.
 int main (){
+// a função "main" é aonde o programa começa de verdade, e o "int" que vem antes dele, está dizendo que o resultado será um numero inteiro.
+// Por sua vez isso afeta o return.
 printf("Hello Word!!!");
   return 0;
+// o comando "return" que por convenção está em 0 representa que "tudo está correto, sem erros.", Caso dê errado, o return tende a dar um numero diferente de 0.
 }
