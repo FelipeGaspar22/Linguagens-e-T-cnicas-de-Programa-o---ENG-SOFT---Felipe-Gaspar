b8d69f912f3ed9f8ea6b13727888672515d090cb
FelipeGaspar22 <estudos.felipe.gaspar@gmail.com>
2026-08-03 08:35:08 -0300

    Create Soma de Números e Primeiras Variaveis.c

---
+++ Soma de Números e Primeiras Variaveis.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1 = 20;
+    int n2 = 15;
+    int soma = n1 + n2;
+    printf("A soma do numero 1 com o numero 2 resulta em %d\n", soma);
+    return 0;
+}
