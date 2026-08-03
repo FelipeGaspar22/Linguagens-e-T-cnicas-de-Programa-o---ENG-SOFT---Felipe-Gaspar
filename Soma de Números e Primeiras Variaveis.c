c3e851c5653c454f5c9c04a296105d62c58470fe
FelipeGaspar22 <estudos.felipe.gaspar@gmail.com>
2026-08-03 08:50:02 -0300

    Update Soma de Números e Primeiras Variaveis.c

--- Soma de Números e Primeiras Variaveis.c
+++ Soma de Números e Primeiras Variaveis.c
@@ -6,7 +6,10 @@ int main(){
     int soma = n1 + n2;
 // A declaração de uma variavel precisa ter o tipo dela definido, antes do seu nome.
     printf("A soma do numero 1 com o numero 2 resulta em %d\n", soma);
-// %d está reservando um espaço dentro do comando "printf", aonde diz que ali naquele espaço, vai entrar um numero inteiro.
-// Por fim "soma" é o valor que vai entrar naquele espaço reservado.
+/*
+%d está reservando um espaço dentro do comando "printf", aonde diz que ali naquele espaço, vai entrar um numero inteiro.
+
+Por fim "soma" é o valor que vai entrar naquele espaço reservado.
+*/
     return 0;
 }
