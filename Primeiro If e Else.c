f19146cef5b1f0ab629047b5467f58d3db6035b0
FelipeGaspar22 <estudos.felipe.gaspar@gmail.com>
2026-08-04 08:16:59 -0300

    Update Primeiro If e Else.c

--- Primeiro If e Else.c
+++ Primeiro If e Else.c
@@ -8,9 +8,13 @@ int main()
     c = 20;
     soma = a + b;
 
+// Acima está o agrupamento das variavéis utilizadas para o programa.    
+    
 if (soma > c){
+// A condição acima afirma que se o valor da soma entre A + B for maior que o valor de C, ele executa o printf abaixo.
     printf("O valor de A = %d\n somado ao valor de B = %d\n resulta em = %d\n, logo a soma de A e B é maior que o valor de C = %d\n!", a,b,soma,c);
 } else {
+// Caso o valor seja memnor ou o igual ao valor de C, executa o printf abaixo.
     printf("O valor de A = %d\n somado a ao valor B = %d\n resulta em = %d\n, logo a soma de A e B é menor que o valor de C = %d\n!", a,b,soma,c);
 }
 
