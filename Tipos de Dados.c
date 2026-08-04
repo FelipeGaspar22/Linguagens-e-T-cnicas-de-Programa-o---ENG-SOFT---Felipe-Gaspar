fd76565d7cb2fcebde408b6c986a7105696ca154
FelipeGaspar22 <estudos.felipe.gaspar@gmail.com>
2026-08-04 08:46:08 -0300

    Create Tipos de Dados.c

---
+++ Tipos de Dados.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+	
+char letra = 'A';
+char nome[] = "Felipe";
+int idade = 18;
+float altura = 1.78;
+double altura_exata = 1.780;
+
+printf("sua letra é: %c\n", letra);
+printf("seu nome é: %s\n", nome);
+printf("sua idade é: %d\n", idade);
+printf("sua altura é: %f\n", altura);
+printf("sua altura exata é: %lf\n", altura_exata);
+
+	
+// Tipos de Dados;
+
+/*
+
+- char; "O valor representado é um caractere de texto, representando um único caractere."
+- char[]; "Uma cadeia de vários caracteres de texto juntos."
+- int; "Valores de números inteiros."
+- float; "Valores de números com ponto flutuante, ou ponto e virgula, menos preciso."
+- double; "Valores de números com ponto flutuante mais preciso."
+- void; "Nenhum tipo, é usado em funções sem retorno."
+
+*/
+
+	return 0;
+}
