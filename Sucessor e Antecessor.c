863dd7742fac6970cedfe2e8271e3990279ee569
FelipeGaspar22 <estudos.felipe.gaspar@gmail.com>
2026-08-10 08:00:01 -0300

    Create Sucessor e Antecessor.c

---
+++ Sucessor e Antecessor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int number;
+    printf("Escolha um número: ");
+    scanf("%d", &number);
+/*
+- A variavél number, está tendo o seu valor alocado pelo comando "scanf", que está lendo o numero digitado pelo usuário, e alocando ele na memória. 
+- O caracter & serve como um referenciado de endereço da memória, ele está dizendo aonde alocar o valor digitado pelo usuário, na memória, e nesse caso está sendo alocado na variavel number.
+*/
+  
+    int sucessor,antecessor;
+    sucessor = number + 1;
+    antecessor = number - 1;
+
+// Logo acima foram declarados as variaveis de sucessor e antecessor, ao qual receber o valor alocado na variavel number, e realizam suas respectivas operações lógicas para chegar ao sucessor e antecessor. 
+  
+printf("O antecessor e sucessor do numero: %d, são = %d seu antecessor e %d seu sucessor", number, antecessor, sucessor);
+
+    return 0;
+}
